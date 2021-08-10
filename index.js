fdab8f5e8fe08e9c5d924ffa83fe9a749cc609f3
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-08-10 08:49:08 -0500

    commit tree aa98a58e0a58c81da6ccec4ed4fc906c01e3571b

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const hq = 42;
+const feet = 264
+
+function distanceFromHqInBlocks(num) {
+        return Math.abs(hq - num)
+    }
+
+function distanceFromHqInFeet(num) {
+    let blocks = distanceFromHqInBlocks(num);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(a, b) { 
+        return Math.abs(a-b) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0
+    }
+    else if (distance <= 2000) {
+        return (distance - 400) * .02
+    }
+    else if (distance < 2500) {
+        return 25
+    }
+    else if (distance > 2500) {
+        return "cannot travel that far"
+    }
+}
